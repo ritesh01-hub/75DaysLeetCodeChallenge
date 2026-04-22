2ec6cea7e3806933ce491a2a1bea24f6ea63ad37
ritesh tiwari <riteshtiwari660@gmail.com>
2026-04-22 09:29:52 +0530

    Time: 0 ms (100%), Space: 22 MB (15.87%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,44 @@
+ 
+class Solution {
+public: 
+ ////method number 1:
+    // long long maxTree(TreeNode* root){
+    //     if(root == NULL) return LLONG_MIN;
+    //     return max((long long)(root->val) , max(maxTree(root->left) , maxTree(root->right)));
+
+    // }
+    // long long  minTree(TreeNode* root){
+    //     if(root == NULL) return LLONG_MAX;
+    //     return min((long long)(root->val) , min(minTree(root->left) ,minTree(root->right)));
+
+    // }
+
+    // bool isValidBST(TreeNode* root) {
+    //     if(root == NULL) return true;
+    //     else if((long long)(root->val) <= maxTree(root->left)) return false;
+    //     else if((long long )(root->val) >= minTree(root->right)) return false;
+    //     return isValidBST(root->left) && isValidBST(root->right);
+    // }
+
+    /// method number 2:
+    void inorder(TreeNode* root , vector<int>& ans){
+        if(root == NULL) return;  // base case 
+        inorder(root->left, ans); // left 
+        ans.push_back(root->val); // root 
+        inorder(root->right ,ans); // right 
+
+    }
+
+
+    bool isValidBST(TreeNode* root) {
+        vector<int> ans;
+        inorder(root , ans);
+        for(int i =1;i<ans.size();i++){
+            if(ans[i]<=ans[i-1]) return false;
+        }
+        return true;
+
+
+    }
+
+};
