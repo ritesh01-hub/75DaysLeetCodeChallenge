15f9b344ac59d967591745497a234c71e3d1c1c1
ritesh tiwari <riteshtiwari660@gmail.com>
2026-05-27 09:04:57 +0530

    Time: 0 ms (100%), Space: 65.1 MB (82.29%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+         vector<int> indegree(n + 1, 0);
+        vector<int> outdegree(n + 1, 0);
+
+        for(auto &t : trust) {
+            int a = t[0];
+            int b = t[1];
+
+            outdegree[a]++; // a trusts someone
+            indegree[b]++;  // b is trusted by someone
+        }
+
+        for(int i = 1; i <= n; i++) {
+            // Judge trusts nobody and everyone trusts judge
+            if(outdegree[i] == 0 && indegree[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
